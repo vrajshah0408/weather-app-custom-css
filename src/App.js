ed4c00bbcfcb599338bbd839469fef00963fc56c
Vraj Shah <vrajshah8@gmail.com>
2023-06-13 19:39:37 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import hotbg from "./assets/hot.jpg";
+import coldbg from "./assets/cold.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <div className="app" style={{ backgroundImage: `url(${coldbg
+    })` }}>
+          <div className="overlay">       
+              <div className="container">
+            <div className="section section__inputs">
+                  <input type="text"
+                    name="city"
+                    placeholder="Enter City..."
+                  />
+                  <button>°F</button>
+                </div>
+    
+                <div className="section section__temperature">
+                  <div className="icon">
+                  <h3>London, GB</h3>
+    
+                    <img
+                    src="https://openweathermap.org/img/wn/10d@2x.png"
+                    alt="Weather Condition Icon"
+                  />
+    
+                    <h3>Cloudy</h3>
+    
+                  </div>
+                  <div className="temperature">
+                    <h1>34 °C</h1>
+    
+                  </div>
+                </div>      
+              
+              </div>            
+          </div>
+        </div>
+      );
+    }
+    
+    export default App;
+    
+    
