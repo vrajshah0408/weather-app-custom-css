0515c91b6c1bfe338539c8a1e3fbe358a1c64535
Vraj Shah <vrajshah8@gmail.com>
2023-06-13 22:44:08 -0400

    Deploy

---
+++ src/weatherService.js
@@ -0,0 +1,40 @@
+/** @format */
+
+const API_KEY = "f32c1454a9a1ed5ed44f064d24cd5208";
+
+const makeIconURL = (iconId) =>
+  `https://openweathermap.org/img/wn/${iconId}@2x.png`;
+
+const getFormattedWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+
+export { getFormattedWeatherData };
